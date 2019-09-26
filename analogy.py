430b35b94914a8b9b6cc0336f4ae249041deb4d8
Roger Ji <roger.ji.32021@gmail.com>
2019-09-25 21:08:12 -0700

    more cli updates

--- analogy.py
+++ analogy.py
@@ -150,28 +150,44 @@ class Analogy:
     def x_log(X_wiki):
         return np.log10(1 + X_wiki, dtype="float32")
 
+    
 test = Analogy()
-response = str(input)
+response = str(input())
 
-while(not any(word in response for word in ['quit', 'exit', 'stop', 'end']):
-      
-max_vocab_words = 1000
-max_context_words = 1000
-X_wiki = Analogy.generate_word_by_context(test.codes, 
+while(not any(word in response for word in ['quit', 'exit', 'stop', 'end'])):   
+    print("Please enter a maximum number of vocab words")
+    succeed = False
+    while(not succeed):
+        try:
+            max_vocab_words = int(input())
+            succeed = True
+        else:
+            print("Please enter a valid maximum number of vocab words (> 0)")
+    succeed = False
+    print("Please enter a maximum number of context words")
+    while (not succeed):
+        try:
+            max_context_words = int(input())
+            succeed = True
+        else:
+            print("Please enter a vfalid maximum number of context words (> 0)")
+            
+    X_wiki = Analogy.generate_word_by_context(test.codes, 
                                   max_vocab_words=max_vocab_words, 
                                   max_context_words=max_context_words, 
                                   context_size=4,
                                   weight_by_distance=True)
-my_vectors = Analogy.reduce(Analogy.x_log(X_wiki), n_components=200)
+    my_vectors = Analogy.reduce(Analogy.x_log(X_wiki), n_components=200)
 
-# save in word2vec format (first line has vocab_size and dimension; other lines have word followed by embedding)
-with codecs.open("my_vectors_200.txt", "w", "utf-8") as f:
-    f.write(str(max_vocab_words) + " " + str(200) + "\n")  
-    for i in range(max_vocab_words):
-        f.write(test.sorted_words[i] + " " + " ".join([str(x) for x in my_vectors[i,:]]) + "\n")
+    # save in word2vec format (first line has vocab_size and dimension; other lines have word followed by embedding)
+    with codecs.open("my_vectors_200.txt", "w", "utf-8") as f:
+        f.write(str(max_vocab_words) + " " + str(200) + "\n")  
+        for i in range(max_vocab_words):
+            f.write(test.sorted_words[i] + " " + " ".join([str(x) for x in my_vectors[i,:]]) + "\n")
 
-# load back in
-word_vectors = KeyedVectors.load_word2vec_format("my_vectors_200.txt", binary=False)
+    # load back in
+    word_vectors = KeyedVectors.load_word2vec_format("my_vectors_200.txt", binary=False)
+    
+    
 
-print("Thank
 print(word_vectors.wv.similar_by_word("red"))
